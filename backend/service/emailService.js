7e29de0404ca47eaecb1d69c8cd4b9640d8600d9
MaxZhylka <dexhonesta@gmail.com>
2024-10-26 17:25:13 +0300

    email service error fixed

--- backend/service/emailService.js
+++ backend/service/emailService.js
@@ -20,7 +20,7 @@ const sendConfirmationEmail=(user,token)=>
             from:'dexhonesta@gmail.com',
             to: user.email,
             subject:'Please confirm your email',
-            text: `Click on the following link to confirm your email:  ${serverAdress}}/confirm?token=${token}`
+            text: `Click on the following link to confirm your email:  ${serverAdress}/confirm?token=${token}`
         }
         return transporter.sendMail(mailOptions);
     }
